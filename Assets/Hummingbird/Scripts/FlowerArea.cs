973d3087b9f0ec02526755ceb63e2c434d734d5b
MinHyung Lee <ss49132002@gmail.com>
2022-06-30 00:27:14 +0900

    feat: flowerArea function

--- Assets/Hummingbird/Scripts/FlowerArea.cs
+++ Assets/Hummingbird/Scripts/FlowerArea.cs
@@ -18,8 +18,63 @@ public class FlowerArea : MonoBehaviour
         {
             float xRotation = UnityEngine.Random.Range(-5f, 5f);
             float yRotation = UnityEngine.Random.Range(-180f, 180f);
-            float yRotation = UnityEngine.Random.Range(-5f, 5f);
+            float zRotation = UnityEngine.Random.Range(-5f, 5f);
+
+            flowerPlant.transform.localRotation = Quaternion.Euler(xRotation, yRotation, zRotation);
         }
+        
+        // Reset each flower
+        foreach (Flower flower in Flowers)
+        {
+            flower.ResetFlower();
+        }
+    }
+
+    public Flower GetFlowerFromNectar(Collider collider)
+    {
+        return nectarFlowerDictionary[collider];
+    }
+
+    private void Awake()
+    {
+        flowerPlants = new List<GameObject>();
+        nectarFlowerDictionary = new Dictionary<Collider, Flower>();
+        Flowers = new List<Flower>();
+    }
+
+    private void Start()
+    {
+        FindChildFlowers(transform);
+    }
     
+    /// <summary>
+    /// Recursively finds all flowers and flower plants that are children of a parent transform
+    /// </summary>
+    /// <param name="parent">The parent of the children to check</param>
+    private void FindChildFlowers(Transform parent)
+    {
+        for (int i = 0; i < parent.childCount; i++)
+        {
+            Transform child = parent.GetChild(i);
+
+            if (child.CompareTag("flower_plant"))
+            {
+                flowerPlants.Add(child.gameObject);
+                FindChildFlowers(child);
+            }
+            else
+            {
+                Flower flower = child.GetComponent<Flower>();
+                if (flower != null)
+                {
+                    Flowers.Add(flower);
+                    nectarFlowerDictionary.Add(flower.nectorCollider, flower);
+                }
+                else
+                {
+                    FindChildFlowers(child);
+                }
+            }
+        }
     }
 }
--- Assets/Hummingbird/Scripts/HummingbirdAgent.cs
+++ Assets/Hummingbird/Scripts/HummingbirdAgent.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.MLAgents;
 using UnityEngine;
 
-public class HummingbirdAgent : MonoBehaviour
+/// <summary>
+/// A hummingbird Machine Learning Agent
+/// </summary>
+public class HummingbirdAgent : Agent
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Tooltip("Force to apply when moving")]
+    public float moveForce = 2f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Tooltip("Speed to pitch up or down")] 
+    public float pitchSpeed = 100f;
+
+    [Tooltip("Speed to rotate around the up axis")]
+    public float yawSpeed = 100f;
+
+    [Tooltip("Transform at the tip of the beak")]
+    public Transform beakTip;
+
+    [Tooltip("The agent's camera")] public Camera agentCamera;
+
+    [Tooltip("Whether this is training mode or gameplay mode")]
+    public bool trainingMode;
+    
+    //The rigidbody of the agent
+    new private Rigidbody rigidbody;
+    
+    //The flower area that the agent is in
+    private FlowerArea flowerArea;
+
+    private Flower nearestFlower;
+
+    private float smoothPitchChange = 0f;
+    private float smoothYawChange = 0f;
+
+    private const float MaxPitchAngle = 80f;
+    private const float BeakTipRadius = 0.008f;
+    
+    //Whether the agent is frozen (intentionally not flying)
+    private bool frozen = false;
+    
+    /// <summary>
+    /// The amount of nectar the agent has obtained this episode
+    /// </summary>
+    public float NectarObtained { get; private set; }
+
 }
