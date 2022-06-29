db06c422f865d1b3fddd2b051a4d15fdc85351a2
MinHyung Lee <ss49132002@gmail.com>
2022-06-30 00:56:27 +0900

    feat: MoveToSafeRandomPosition

--- Assets/Hummingbird/Scripts/HummingbirdAgent.cs
+++ Assets/Hummingbird/Scripts/HummingbirdAgent.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using Unity.MLAgents;
 using UnityEngine;
 
@@ -47,4 +48,88 @@ public class HummingbirdAgent : Agent
     /// </summary>
     public float NectarObtained { get; private set; }
 
+    public override void Initialize()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        flowerArea = GetComponentInParent<FlowerArea>();
+        
+        // If not training mode, no max step, play forever
+        if (!trainingMode) MaxStep = 0;
+        
+
+    }
+    
+    /// <summary>
+    /// Reset the agent when an episode begins
+    /// </summary>
+    public override void OnEpisodeBegin()
+    {
+        if (trainingMode)
+        {
+            //Only reset flowers in training when there is one agent per area
+            flowerArea.ResetFlowers();
+        }
+        
+        // Reset nectar obtained
+        NectarObtained = 0f;
+
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.angularVelocity = Vector3.zero;
+        
+        // Default to spawning in front of a flower
+        bool inFrontOfFlower = true;
+        if (trainingMode)
+        {
+            inFrontOfFlower = UnityEngine.Random.value > .5f;
+        }
+
+        MoveToSafeRandomPosition(inFrontOfFlower);
+
+        UpdateNearestFlower();
+
+    }
+
+    private void MoveToSafeRandomPosition(bool inFrontOfFlower)
+    {
+        bool safePositionFound = false;
+        int attemptsRemaining = 100;
+        Vector3 potentialPosition = Vector3.zero;
+        Quaternion potentialRotation = new Quaternion();
+
+        while (!safePositionFound && attemptsRemaining > 0)
+        {
+            attemptsRemaining--;
+            if (inFrontOfFlower)
+            {
+                Flower randomFlower = flowerArea.Flowers[UnityEngine.Random.Range(0, flowerArea.Flowers.Count)];
+
+                float distanceFromFlower = UnityEngine.Random.Range(.1f, .2f);
+                potentialPosition = randomFlower.transform.position + randomFlower.FlowerUpVector * distanceFromFlower;
+
+                Vector3 toFlower = randomFlower.FlowerCenterPosition - potentialPosition;
+                potentialRotation = Quaternion.LookRotation(toFlower, Vector3.up);
+            }
+            else
+            {
+                float height = UnityEngine.Random.Range(1.2f, 2.5f);
+                float radius = UnityEngine.Random.Range(2f, 7f);
+                Quaternion direction = quaternion.Euler(0f, UnityEngine.Random.Range(-180f, 180f), 0f);
+                potentialPosition = flowerArea.transform.position + Vector3.up * height +
+                                    direction * Vector3.forward * radius;
+
+                float pitch = UnityEngine.Random.Range(-60f, 60f);
+                float yaw = UnityEngine.Random.Range(-180f, 180f);
+                potentialRotation = Quaternion.Euler(pitch, yaw, 0f);
+            }
+
+            Collider[] colliders = Physics.OverlapSphere(potentialPosition, 0.05f);
+
+            safePositionFound = colliders.Length == 0;
+        }
+
+        Debug.Assert(safePositionFound, "Could not find a safe position to spawn");
+
+        transform.position = potentialPosition;
+        transform.rotation = potentialRotation;
+    }
 }
